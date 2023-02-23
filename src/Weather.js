6a684297c9dbe288d7eec42f409ecae464e498bd
RazaNaqsh <owaisraza1704@gmail.com>
2023-02-24 00:07:33 +0530

    Export Weather function in Weather module

---
+++ src/Weather.js
@@ -0,0 +1,17 @@
+async function getWeather(place) {
+	const resp = await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=c71a2bee0691fb692d156e4c950c6d61`
+	);
+	// console.log(resp);
+
+	const respData = await resp.json();
+	console.log(respData);
+
+	const weatherMainData = respData.main;
+	console.log(weatherMainData);
+
+	const weatherSubData = respData.weather[0];
+	console.log(weatherSubData);
+}
+
+export default getWeather;
