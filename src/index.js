2b4ab37525e473af61f3e3cccf21eaeaf51ece9b
RazaNaqsh <owaisraza1704@gmail.com>
2023-02-23 23:54:39 +0530

    Function for getting weather data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import "./scss/style.scss";
 
 console.log("webpack here");
+
+async function getWeather(place) {
+	const resp = await fetch(
+		`https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=c71a2bee0691fb692d156e4c950c6d61`
+	);
+	console.log(resp);
+
+	const respData = await resp.json();
+	console.log(respData);
+
+	const weatherData = respData.main;
+	console.log(weatherData);
+}
+
+getWeather("Jamshedpur");
